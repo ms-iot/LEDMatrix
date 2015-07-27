440c48022cd076347cbd8467cfdff749f32672ae
superlime <github@superlime.com>
2015-07-27 12:59:37 -0700

    Hack around missing sendBlob

--- UWP/RemoteLedMatrix/Helpers/FirmataExtensions.cs
+++ UWP/RemoteLedMatrix/Helpers/FirmataExtensions.cs
@@ -9,33 +9,39 @@ namespace RemoteLedMatrix.Helpers
 {
     public static class FirmataExtensions
     {
+        public const byte SYSEX_BLOB_COMMAND = 0x7C; // send a series of 7-bit resolution characters
+        public const byte START_SYSEX = 0xF0; // start a MIDI Sysex message
+        public const byte END_SYSEX = 0xF7; // end a MIDI Sysex message
+
         public static void sendSysex(this UwpFirmata firmata, byte command)
         {
             firmata.beginSysex(command);
             firmata.endSysex();
         }
 
-        public static void sendBlob(this UwpFirmata firmata, IEnumerable<byte> bytes)
+        public static void sendPixelBlob(this UwpFirmata firmata, IEnumerable<byte> bytes)
         {
-            firmata.beginBlob();
+            firmata.write(START_SYSEX);
+            firmata.write(SYSEX_BLOB_COMMAND);
 
             foreach (byte b in bytes)
             {
-                firmata.appendBlob(b);
+                firmata.write(b);
             }
 
-            firmata.endBlob();
+            firmata.write(END_SYSEX);
         }
 
-        public static void sendBlob(this UwpFirmata firmata, IEnumerable<byte> bytes, int inSetsOf)
+        public static void sendPixelBlob(this UwpFirmata firmata, IEnumerable<byte> bytes, int inSetsOf)
         {
             if (inSetsOf == 0)
             {
-                firmata.sendBlob(bytes);
+                firmata.sendPixelBlob(bytes);
                 return;
             }
 
-            bytes.InSetsOf(inSetsOf).ForEach(firmata.sendBlob);
+            bytes.InSetsOf(inSetsOf).ForEach(firmata.sendPixelBlob);
+            firmata.flush();
         }
     }
 }
