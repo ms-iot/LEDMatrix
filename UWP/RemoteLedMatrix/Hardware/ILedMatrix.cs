b7d9249fbe3aa7ff080a672bc9d57ed567654085
superlime <github@superlime.com>
2015-07-27 15:22:48 -0700

    More cleanup

--- UWP/RemoteLedMatrix/Hardware/ILedMatrix.cs
+++ UWP/RemoteLedMatrix/Hardware/ILedMatrix.cs
@@ -1,19 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.Graphics.Imaging;
-using Windows.UI.Xaml.Media.Imaging;
-
+﻿
 namespace RemoteLedMatrix.Hardware
 {
+    using System.Threading.Tasks;
+    using Windows.UI.Xaml.Media.Imaging;
+
+    /// <summary>
+    /// Interface for dealing with different LED matrix hardware types
+    /// </summary>
     public interface ILedMatrix
     {
-        void Initialize();
-        Task DisplayImage(WriteableBitmap image);
-
+        /// <summary>
+        /// Gets a value representing the number of pixels wide the matrix is
+        /// </summary>
+        /// <value>
+        /// A value representing the number of pixels wide matrix is
+        /// </value>
         int PixelWidth { get; }
+
+        /// <summary>
+        /// Gets a value representing the number of pixels high the matrix is
+        /// </summary>
+        /// <value>
+        /// A value representing the number of pixels high the matrix is
+        /// </value>
         int PixelHeight { get; }
+
+        /// <summary>
+        /// Sends a command to initialize the LED matrix
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Displays an image on the LED matrix
+        /// </summary>
+        /// <param name="image">Bitmap to display on the LED matrix</param>
+        /// <returns>Task for tracking the status of the async call</returns>
+        Task DisplayImage(WriteableBitmap image);
     }
 }
