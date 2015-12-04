8e174f2ae228db55d019090cad8e78d8db8e9a61
Matthew McKee <matt_mckee@outlook.com>
2015-12-03 21:05:59 -0800

    remove unused variable

--- UWP/RemoteLedMatrix/MainPage.xaml.cs
+++ UWP/RemoteLedMatrix/MainPage.xaml.cs
@@ -546,7 +546,6 @@ namespace RemoteLedMatrix
         /// <returns>true if connection succeeded.</returns>
         public async Task Connect(Connection selectedConnection)
         {
-            bool result = false;
             if (this.currentConnection != null)
             {
                 App.SerialStream.end();
